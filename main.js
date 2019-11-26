89becb0869af8822be1117fa3c04f222b6e522e6
weilinh98 <weilinhong007@gmail.com>
2019-11-26 13:20:40 -0800

    finish handle click, horizontal and vertical check

--- main.js
+++ main.js
@@ -1,59 +1,65 @@
-
-// function checkHorizontal(){
-//   var Horizontal = 0;
-//   for(right side===){
-//     Horizontal++
-//   }
-//   for(left side===){
-//     Horizontal++
-//   }
-//   if(...)
-
-//   return Horizontal
-// }
+$(document).ready(initializeApp);
 
 var gameboard = [{}, {}, {}, {}, {}, {}, {}]
-var playerTurnColor = "red";
+var playerTurnColor = "yellow";
 var clickedColumnNumber = null;
-var rowNumber = null;
+var clickedRowNumber = null;
+var horizontalMatch = 0;
+var verticalMatch = 0;
+var rightDiagnal = 0;
+var leftDiagnal = 0;
 
-$('col0').on('click', handleClick);
-$('col1').on('click', handleClick);
-$('col2').on('click', handleClick);
-$('col3').on('click', handleClick);
-$('col4').on('click', handleClick);
-$('col5').on('click', handleClick);
-$('col6').on('click', handleClick);
-
-function handleClick(event) {
-  clickedColumnNumber = $(parseInt(event.currentTarget.attr('class')).split('')[3]);
-  rowNumber= gameboard[clickedColumnNumber].length;
-  var targetbox = null;
-  if (Object.keys(gameboard[clickedColumnNumber]).length < 7) {
-    gameboard[clickedColumnNumber].rowNumber = playerTurnColor;
-    targetbox = $(event.currentTarget > "#row" + rowNumber).addClass(playerTurnColor);
-    if (playerTurnColor === "red") {
-      playerTurnColor === "yellow"
-    }
-    else {
-      playerTurnColor === "red"
-    }
-  }
-  var box = clickedColumnNumber
+function initializeApp() {
+  $('.col0').on('click', handleClick);
+  $('.col1').on('click', handleClick);
+  $('.col2').on('click', handleClick);
+  $('.col3').on('click', handleClick);
+  $('.col4').on('click', handleClick);
+  $('.col5').on('click', handleClick);
+  $('.col6').on('click', handleClick);
 }
 
-//horizontal check
+function handleClick(event) {
+  clickedColumnNumber = parseInt($((event.currentTarget)).attr('class').split('')[3]);
+  clickedRowNumber = Object.keys(gameboard[clickedColumnNumber]).length;
+  if (clickedRowNumber < 7) {
+    gameboard[clickedColumnNumber][clickedRowNumber] = playerTurnColor;
+    $("." + $(event.currentTarget).attr('class') + ">" +".row" + clickedRowNumber).addClass(playerTurnColor);
+    if (playerTurnColor === "yellow") {
+      playerTurnColor = "red"
+    }
+    else {
+      playerTurnColor = "yellow"
+    }
+    check();
+  }
+}
+
+// horizontal check
 
 function check(){
-  var targetProperty = gameboard[clickedColumnNumber].rowNumber;
-  var horizontalMatch = 0;
-  var verticalMatch = 0;
-  for(var columnIndex = 0; columnIndex < 6; columnIndex++){
-    if(targetProperty === gameboard[columnIndex].rowNumber){
+  var targetProperty = gameboard[clickedColumnNumber][clickedRowNumber];
+  for(var columnIndex = 0; columnIndex < 7; columnIndex++){
+    if(targetProperty === gameboard[columnIndex][clickedRowNumber]){
       horizontalMatch++;
+      if(horizontalMatch === 4){
+        break;
+      }
     }
     else{
       horizontalMatch = 0;
+    }
+  }
+  // vertical check
+  for(var rowIndex = 0; rowIndex < 6; rowIndex++){
+    if(targetProperty === gameboard[clickedColumnNumber][rowIndex]){
+      verticalMatch++;
+      if(verticalMatch === 4){
+        break;
+      }
+    }
+    else{
+      verticalMatch = 0;
     }
   }
 
